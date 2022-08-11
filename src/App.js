da201bbd2b576b94b4e81ae1ae6663478aa73f35
Emil Babazade <emilbabazade29@gmail.com>
2022-08-11 17:20:59 +0400

    wefewfewfewf

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const AddNewPerson = ({
+  name, handleNameChange,
+  phone, handlePhoneChange,
+  handleSubmit
+}) => (
+  <form>
+    <div>
+      name: <input value={name} onChange={handleNameChange} />
     </div>
-  );
+    <div>
+      phone: <input value={phone} onChange={handlePhoneChange} />
+    </div>
+    <div>
+      <button type="submit" onClick={handleSubmit}>add</button>
+    </div>
+  </form>
+)
+
+const Numbers = ({persons}) => (
+  <div>
+    {persons.map(p => <p>{p.name} {p.phone}</p>)}
+  </div>
+)
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', phone: '12-232-2324234' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
+
+  const handleInputChange = (setter) => {
+    return evt => {
+      const value = evt.target.value;
+      setter(value);
+    }
+  }
+
+  const addNewPerson = evt => {
+    evt.preventDefault();
+    const isDuplicate = persons.find(p => p.name.toLowerCase().trim() === newName.toLowerCase().trim());
+    if(isDuplicate) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+    setPersons([...persons, {name: newName, phone: newPhone}]);
+    setNewName('');
+    setNewPhone('');
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <h3>Add a new</h3>
+      <AddNewPerson
+        handleNameChange={handleInputChange(setNewName)}
+        handlePhoneChange={handleInputChange(setNewPhone)}
+        handleSubmit={addNewPerson}
+        name={newName}
+        phone={newPhone} />      
+      <h3>Numbers</h3>
+      <Numbers persons={persons} />
+    </div>
+  )
 }
 
-export default App;
+export default App
